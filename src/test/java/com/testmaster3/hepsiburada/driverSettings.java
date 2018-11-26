1026af484e09e6a4acd5a3ab6d2dd5df7967bbd6
AhmetDemirel <ahmetdemirel094@gmail.com>
2018-11-26 09:53:38 +0300

    Update driverSettings.java

--- src/test/java/com/testmaster3/hepsiburada/driverSettings.java
+++ src/test/java/com/testmaster3/hepsiburada/driverSettings.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.CapabilityType;
+
 
 import java.util.concurrent.TimeUnit;
 
